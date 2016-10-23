ee594fe799a8f248cdde420cbe9a87b88918ce13
LongPotato <bkh4nh@gmail.com>
2016-10-22 18:11:22 -0700

    created server folder structure

---
+++ server/public/scripts/app.js
@@ -0,0 +1,6 @@
+var app = angular.module('App', []);
+
+app.controller('Controller', function($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+})
---
+++ server/templates/scripts/app.js
@@ -0,0 +1,4 @@
+var app = angular.module('App', []);
+
+app.controller('Controller', function($scope) {
+});
