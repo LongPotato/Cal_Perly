7369da728b0e570ca0af89afdd74a84abf5b0842
Ubuntu <ubuntu@ip-172-31-28-220.us-west-2.compute.internal>
2016-11-02 01:18:57 +0000

    added script to get Spring17 class info

--- modules/hello.pl
+++ modules/hello.pl
@@ -2,5 +2,11 @@ use strict;
 use warnings;
 use Data::Dumper;
 use SearchTweet;
+use Schedule;
+
 
 print Dumper SearchTweet::get_tweets('calpolypomona');
+
+my $hash_ref=Schedule::getSchedule();
+
+print Dumper (\$hash_ref);
