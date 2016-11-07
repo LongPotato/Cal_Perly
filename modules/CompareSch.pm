f0c2e3d8c233428888a8476dcf73944787d149ff
Miraj Patel <mspatel@cpp.edu>
2016-11-07 10:26:37 -0800

    compare schedule

---
+++ modules/CompareSch.pm
@@ -0,0 +1,30 @@
+package CompareSch;
+use strict;
+use warnings;
+use Schedule;
+use Storable qw(dclone);
+use Data::Dumper;
+
+
+sub compareSchedule{
+  my ($completed, $scheduleInfo) = @_;
+  my @completed_courses = @{$completed};
+  my $schedule = dclone($scheduleInfo);
+
+
+  for my $done (@{$completed_courses[0]}){
+        my $title = $done->{'title'};
+        for my $section (keys %$schedule){
+                if($schedule->{$section}{'Course'} eq $title){
+                        delete $schedule->{$section};
+                }
+
+        }
+
+}
+#print $fileh  Dumper ($schedule);
+
+return $schedule;
+}
+1;
+    
--- modules/hello.pl
+++ modules/hello.pl
@@ -3,9 +3,26 @@ use warnings;
 use Data::Dumper;
 use SearchTweet;
 use Schedule;
+use CompareSch;
 
 print Dumper SearchTweet::get_tweets('calpolypomona');
 
 my $hash_ref = Schedule::getSchedule();
+my $courses = [[
+		{
+			'title' => 'CS 141',
+		},
+		{
+			'title' => 'CS 241',
+		},
+		{
+			'title' => 'CS 140',
+		},
+		{
+			'title' => 'CS 256',
+		},
+		]];
 
-print Dumper (\$hash_ref);
+my $schedule = CompareSch::compareSchedule($courses,$hash_ref);
+
+print Dumper (\$schedule);
